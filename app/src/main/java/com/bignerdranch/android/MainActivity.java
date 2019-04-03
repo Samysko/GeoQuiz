6b9b4f85ad148d5502463686b748c81349a6c45b
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-04-02 20:06:40 -0600

    Toast gravity testing
    Testing which is the behavior of the static variables of Gravity and which effect have inside the Toast

--- app/src/main/java/com/bignerdranch/android/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/MainActivity.java
@@ -21,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this,
-                        R.string.correct, Toast.LENGTH_LONG).show();
 
-//                CHALLENGE: Brief message to the final user that indicates the correct answer
-//                at the top center of the activity
-//                Toast toast = Toast.makeText(MainActivity.this, R.string.true_button,
-//                        Toast.LENGTH_LONG);
-//                toast.setGravity(Gravity.TOP, Gravity.AXIS_X_SHIFT, Gravity.AXIS_Y_SHIFT);
-//                toast.show();
+                //This code is part of the chapter code, not part of the challenge
+//                Toast.makeText(MainActivity.this,
+//                        R.string.correct, Toast.LENGTH_LONG).show();
+
+                //CHALLENGE: Brief message to the final user that indicates the correct answer
+                //at the top center of the activity
+                Toast toast = Toast.makeText(MainActivity.this, R.string.true_button,
+                        Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, Gravity.AXIS_X_SHIFT, Gravity.AXIS_Y_SHIFT);
+                toast.show();
             }
         });
 
@@ -37,8 +39,17 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
            @Override
             public void onClick(View v){
-               Toast.makeText(MainActivity.this,
-                       R.string.incorrect, Toast.LENGTH_LONG).show();
+               //This code is part of the chapter code, not part of the challenge
+//               Toast.makeText(MainActivity.this,
+//                       R.string.incorrect, Toast.LENGTH_LONG).show();
+
+               //CHALLENGE: Brief message to the final user that indicates the incorrect answer
+               //at the top center of the activity
+               Toast toast = Toast.makeText(MainActivity.this,
+                       R.string.incorrect, Toast.LENGTH_LONG);
+               toast.setGravity(Gravity.TOP, Gravity.CENTER_HORIZONTAL, Gravity.CENTER_VERTICAL);
+               toast.show();
+
            }
         });
     }
