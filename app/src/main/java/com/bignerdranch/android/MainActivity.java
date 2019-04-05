ab3e5edc922e8340c9850ada7c1db64924365bb5
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-04-04 19:22:28 -0600

    Model layer was added with the Question class and changes to the View layer
    Question class was added as a model, a Next button was added and an Id for the question TextView

--- app/src/main/java/com/bignerdranch/android/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/MainActivity.java
@@ -2,7 +2,6 @@ package com.bignerdranch.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
