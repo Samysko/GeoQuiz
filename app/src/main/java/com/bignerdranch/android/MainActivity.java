de8abef45b1a1ccaeeeb9a6a99a8e1081cb008ca
Samuel Bernal <samuelisaacbernal@hotmail.com>
2019-04-04 19:57:01 -0600

    NextButton added with its own listener and correction to the question resources
    NextButton capable of change the current question inside the QuestionTextView.
    
    Correciton made to the Strings resources adding question_oceans which was missing

--- app/src/main/java/com/bignerdranch/android/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/MainActivity.java
@@ -4,16 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    Button mTrueButton;
-    Button mFalseButton;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    Question[] mQuestionBank = {new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instance of the next button with its own listener
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
         //Instance of the true button with its own listener
         mTrueButton = findViewById(R.id.true_button);
@@ -25,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Instance of the false button with its own listener
         mFalseButton = findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
            @Override
@@ -33,5 +59,13 @@ public class MainActivity extends AppCompatActivity {
                        R.string.incorrect, Toast.LENGTH_LONG).show();
            }
         });
+
+        updateQuestion();
+    }
+
+    //Method to update the current question inside the QuestionTextView
+    public void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
     }
 }
